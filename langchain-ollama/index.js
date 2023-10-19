96c32c30e5009a3c141b3be42b072d29eacf661c
Ross Uber <rossuber@gmail.com>
2023-10-18 23:16:01 -0400

    Add files via upload

---
+++ langchain-ollama/index.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { Ollama } from "langchain/llms/ollama";
+import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import "@tensorflow/tfjs-node";
+import { TensorFlowEmbeddings } from "langchain/embeddings/tensorflow";
+import { RetrievalQAChain } from "langchain/chains";
+
+const app = express();
+const port = 8080;
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/', async (req, res) => {
+  const theData = req.body;
+
+  console.log(theData.model);
+  console.log(theData.input);
+  console.log(theData.langchainURL);
+
+  const ollama = new Ollama({
+    baseUrl: "http://localhost:11434",
+    model: theData.model,
+    temperature: theData.temperature,
+    topP: theData.topp
+  });
+
+  const loader = new CheerioWebBaseLoader( theData.langchainURL );
+  const data = await loader.load();
+      
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 20
+  });
+  const splitDocs = await textSplitter.splitDocuments(data);
+      
+  const vectorStore = await MemoryVectorStore.fromDocuments(splitDocs, new TensorFlowEmbeddings());
+
+  const retriever = vectorStore.asRetriever();
+  const chain = RetrievalQAChain.fromLLM(ollama, retriever);
+
+  const result = await chain.call({query: theData.input});
+  console.log(result)
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
